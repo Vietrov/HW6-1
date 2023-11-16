b3e0a16ed4ddc5d97aaec9ce518af4b5907a4808
PC <PC@DESKTOP-R4P1PJC>
2023-11-16 22:28:33 +0100

    Add project files.

---
+++ HW6-1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static double Power(double baseNumber, int exponent)
+        {
+            
+            if (exponent == 0)
+            {
+                return 1;
+            }
+
+            
+            if (exponent < 0)
+            {
+                baseNumber = 1 / baseNumber;
+                exponent = -exponent;
+            }
+
+            double result = 1;
+            for (int i = 0; i < exponent; i++)
+            {
+                result *= baseNumber;
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите основание степени:");
+            double baseNumber = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите показатель степени:");
+            int exponent = Convert.ToInt32(Console.ReadLine());
+
+            double result = Power(baseNumber, exponent);
+            Console.WriteLine($"Результат: {result}");
+        }
+    }
+}
